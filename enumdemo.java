4b36da305838e35f51ecb928f12c7e345d290353
Yash Deep <143354431+yashdeep370@users.noreply.github.com>
2025-01-28 22:28:11 +0530

    Add files via upload

---
+++ enumdemo.java
@@ -0,0 +1,18 @@
+enum Dept{
+    CS,IT,CIVIL,ECE
+}
+
+public class enumdemo {
+        public static void main(String[] args) {
+            Dept d = Dept.CIVIL;
+            
+            // System.out.println(d.ordinal());
+            // System.out.println(d.name());
+            // System.out.println(Dept.valueOf("IT"));
+  
+            // Dept list[] = Dept.values();
+            
+            // for(Dept x:list)
+            // System.out.println(x);
+        }
+}
